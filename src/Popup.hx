7a9a21c3ca881cd437e2247483d96f6e738c2375
235 <hotup@qq.com>
2016-03-22 00:45:07 +0800

    移除 popup页面的 JQuery

--- src/Popup.hx
+++ src/Popup.hx
@@ -1,12 +1,12 @@
 package;
 
 import haxe.Constraints.Function;
-import js.Browser;
 import js.Lib;
-import js.jquery.JQuery;
+import js.Browser;
 import js.Browser.window;
 import js.Browser.document;
 import js.html.MouseEvent;
+import js.html.Element;
 import js.html.URL;
 import chrome.Extension;
 import chrome.Tabs;
@@ -19,24 +19,20 @@ class Popup{
 
 	static inline var ACTIVE = "active";
 	static var ps:Ps;	// reference from Background
-	static var redirect:JQuery;
-	static var block:JQuery;
+
+	static var redirect:Element;
+	static var block:Element;
 
 	public static function main(){
 		Background.init(Extension.getBackgroundPage());
 		ps = Background.ps;
-
-
 		document.querySelector("#main").onclick = onClick;
-		redirect = new JQuery("#main button.net-redirect:first");
-		block = new JQuery("#main button.net-block:first");
-		state(redirect, ps.redirect);
-		state(block, ps.block);
+		redirect = document.querySelector("#main button.net-redirect");
+		block = document.querySelector("#main button.net-block");
+		redirect.classList.toggle(ACTIVE, ps.redirect);
+		block.classList.toggle(ACTIVE, ps.block);
 	}
 
-	static function state(node:JQuery, b:Bool):Void{
-		b ? node.addClass(ACTIVE) : node.removeClass(ACTIVE);
-	}
 
 	static function onClick(e:MouseEvent):Void{
 		var tar:js.html.DOMElement = cast e.target;
@@ -58,13 +54,13 @@ class Popup{
 			switch(tar.id){
 				case "btn_redirect":
 					ps.redirect = !ps.redirect;
-					state(redirect, ps.redirect);
+					redirect.classList.toggle(ACTIVE, ps.redirect);
 					Background.netRedirect(ps.redirect);
 					Storage.sync.set(ps);
 
 				case "btn_block":
 					ps.block = !ps.block;
-					state(block, ps.block);
+					block.classList.toggle(ACTIVE, ps.block);
 					Background.netBlock(ps.block);
 					Storage.sync.set(ps);
 				default:
